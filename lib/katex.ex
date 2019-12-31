510c223e02af2578e543d88c1e1cebcfd2d0515a
ecialo <ecialo@yandex.ru>
2020-01-01 00:38:24 +0300

    first

---
+++ lib/katex.ex
@@ -0,0 +1,18 @@
+defmodule Katex do
+  @moduledoc """
+  Documentation for Katex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Katex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/katex_test.exs
@@ -0,0 +1,8 @@
+defmodule KatexTest do
+  use ExUnit.Case
+  doctest Katex
+
+  test "greets the world" do
+    assert Katex.hello() == :world
+  end
+end
