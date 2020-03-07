c18e0b1a05049646e459f299d4c6f844641512e5
ecialo <ecialo@yandex.ru>
2020-03-07 22:43:07 +0300

    tests for away state

--- lib/mauricio/catchat/cat/state/away.ex
+++ lib/mauricio/catchat/cat/state/away.ex
@@ -1,7 +1,6 @@
 defmodule Mauricio.CatChat.Cat.State.Away do
   alias __MODULE__, as: Away
   alias Mauricio.Text
-  alias Mauricio.CatChat.{Cat, Member}
   alias Mauricio.CatChat.Cat.{State, CatState}
   alias Mauricio.CatChat.Cat.State.Awake
 
@@ -13,14 +12,14 @@ defmodule Mauricio.CatChat.Cat.State.Away do
 
   defimpl CatState do
     def pet(%Away{}, cat, who),
-      do: {cat, who, Text.get_text(:away_pet, who: who)}
+      do: {cat, nil, Text.get_text(:away_pet, who: who)}
     def hug(%Away{}, cat, who),
-      do: {cat, who, Text.get_text(:hug_away, who: who)}
+      do: {cat, nil, Text.get_text(:hug_away, who: who)}
     def mew(%Away{}, _cat, _who), do: nil
     def loud_sound_reaction(%Away{}, _cat, _who), do: nil
     def tire(%Away{}, _cat, _who), do: nil
     def pine(%Away{}, cat, who),
-      do: {%{cat | state: Awake.new}, who, Text.get_text(:cat_is_back)}
+      do: {%{cat | state: Awake.new}, who, Text.get_text(:cat_is_back, cat: cat, who: who)}
     defdelegate react_to_triggers(state, cat, who, triggers), to: State
     def eat(%Away{}, _cat, _who), do: nil
     def hungry(%Away{}, _cat, _feeder), do: nil
---
+++ test/cat/state/away_test.exs
@@ -0,0 +1,41 @@
+defmodule MauricioTest.Cat.State.Awake do
+  use ExUnit.Case
+
+  alias Mauricio.CatChat.{Cat, Member}
+  alias Mauricio.CatChat.Cat.State.{Awake, Away}
+  alias Mauricio.Text
+
+  alias MauricioTest.Helpers
+
+  test "pet" do
+    member = Member.new("A", "B", 1, 1, true)
+    cat = Cat.new("C", Away.new, 1, 1, 0)
+
+    expected = Text.get_all_texts(:away_pet, who: member, cat: cat)
+    {cat, _member, text} = Cat.pet(cat, member)
+
+    assert Helpers.weak_text_eq(text, expected)
+    assert cat.times_pet == 0
+  end
+
+  test "hug" do
+    member = Member.new("A", "B", 1, 1, true)
+    cat = Cat.new("C", Away.new, 1, 1, 0)
+
+    expected = Text.get_all_texts(:hug_away, who: member, cat: cat)
+    {cat, _member, text} = Cat.hug(cat, member)
+
+    assert Helpers.weak_text_eq(text, expected)
+  end
+
+  test "pine" do
+    member = Member.new("A", "B", 1, 1, true)
+    cat = Cat.new("C", Away.new, 1, 1, 0)
+
+    expected = Text.get_all_texts(:cat_is_back, who: member, cat: cat)
+    {cat, _member, text} = Cat.pine(cat, member)
+
+    assert Helpers.weak_text_eq(text, expected)
+    assert cat.state == Awake.new()
+  end
+end
