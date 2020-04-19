41f283312a0290932cdb6281ae04e9aac498edfe
Ivan Danilychev <eveningsteps@gmail.com>
2020-04-19 05:49:34 +0200

    Don't make up last names if they don't exist. Also, fix typos.

--- test/cat_test.exs
+++ test/cat_test.exs
@@ -4,13 +4,14 @@ defmodule MauricioTest.CatTest do
   alias Mauricio.CatChat.Cat
   alias Mauricio.CatChat.Member
   alias Mauricio.CatChat.Cat.State.{Awake, Away, WantCare}
+  alias Nadia.Model.User, as: NadiaUser
 
   test "joyful pet" do
     member = Member.new("A", "B", 1, 1, True)
     cat = Cat.new("C", WantCare.new(), 1, 1, 0)
 
     expected_text = """
-    <i>A B погладил котика</i>
+    <i>A B гладит котика.</i>
     Мурррррррр.
     """
 
@@ -34,4 +35,18 @@ defmodule MauricioTest.CatTest do
     assert cat.times_pet == 0
   end
 
+  test "user with only the first name is handled properly" do
+    expected_text = """
+    <i>One хочет погладить котяру, но обнаруживает, что того нет дома.</i>
+    """
+    cat = Cat.new("Mau", Away.new, 1, 1, 0)
+    for member <- [
+      Member.new("One", nil, 1),
+      Member.new("One", nil, 1, 1, True),
+      Member.new(%NadiaUser{first_name: "One", last_name: nil, id: 1}),
+    ] do
+      {_cat, _member, text} = Cat.pet(cat, member)
+      assert text == expected_text
+    end
+  end
 end
