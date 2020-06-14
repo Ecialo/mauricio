567c9f62b2d0986c04eee448db9b6ab89ae867c3
ecialo <ecialo@yandex.ru>
2020-06-14 17:29:39 +0300

    Provide default values for storage config

--- config/test.exs
+++ config/test.exs
@@ -4,4 +4,10 @@ config :nadia,
   base_url: "http://localhost:32002/"
 
 config :mauricio,
-  storage: [type: :mongo, url: "mongodb://" <> System.get_env("MONGODB_HOST") <> ":" <> System.get_env("MONGODB_PORT") <> "/db-name"]
+  storage: [
+    type: :mongo,
+    url:
+      "mongodb://" <>
+        System.get_env("MONGODB_HOST", "localhost") <>
+        ":" <> System.get_env("MONGODB_PORT", "27017") <> "/db-name"
+  ]
